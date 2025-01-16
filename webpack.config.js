65dc66253972dbd7966eeb152b586293e728767c
venjye <venjye.wang@dingflow.com>
2025-01-16 18:46:56 +0800

    20250116184656

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html'
+    })
+  ],
   resolve: {
     fallback: {
       buffer: require.resolve('buffer/'),
